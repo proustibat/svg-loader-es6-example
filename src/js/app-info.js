89e3bbd9e789066c43172eb6688129ba540b668f
Proustibat <jennifer.proust@gmail.com>
2018-01-10 21:22:14 +0100

    build(svg-loader-es6): update lib to get i on npm

--- src/js/app-info.js
+++ src/js/app-info.js
@@ -1,1 +1,1 @@
-window.AppInfo = { name: 'svg-loader-es6-example', version: '1.0.0', build: '1515613185836', dateBuild: '2018-01-10T19:39:45.836Z' };
+window.AppInfo = { name: 'svg-loader-es6-example', version: '1.0.0', build: '1515615672146', dateBuild: '2018-01-10T20:21:12.146Z' };
