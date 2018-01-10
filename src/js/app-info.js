c0004392105286b7b3bb0eaa93faff0f78644717
Proustibat <jennifer.proust@gmail.com>
2018-01-10 20:59:40 +0100

    feat(Media Query): Display generator differently on mobile devices

--- src/js/app-info.js
+++ src/js/app-info.js
@@ -1,1 +1,1 @@
-window.AppInfo = { name: 'svg-loader-es6-example', version: '1.0.0', build: '1515608529779', dateBuild: '2018-01-10T18:22:09.779Z' };
+window.AppInfo = { name: 'svg-loader-es6-example', version: '1.0.0', build: '1515613185836', dateBuild: '2018-01-10T19:39:45.836Z' };
