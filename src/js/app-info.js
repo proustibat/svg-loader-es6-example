15dfeaca55d6edffd40f4ef5220ca8bca12741e5
Proustibat <jennifer.proust@gmail.com>
2018-01-11 18:25:04 +0100

    feat(Dashboard): Add badges from code quality tools and CI tools

--- src/js/app-info.js
+++ src/js/app-info.js
@@ -1,1 +1,1 @@
-window.AppInfo = { name: 'svg-loader-es6-example', version: '1.0.0', build: '1515687966528', dateBuild: '2018-01-11T16:26:06.528Z' };
+window.AppInfo = { name: 'svg-loader-es6-example', version: '1.0.0', build: '1515688942743', dateBuild: '2018-01-11T16:42:22.743Z' };
