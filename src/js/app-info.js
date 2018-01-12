f078db643049dda83f5cb96cdee24d42c8e271d7
Proustibat <jennifer.proust@gmail.com>
2018-01-12 11:49:01 +0100

    feat(Background svg): transition css

--- src/js/app-info.js
+++ src/js/app-info.js
@@ -1,1 +1,1 @@
-window.AppInfo = { name: 'svg-loader-es6-example', version: '1.0.0', build: '1515747963261', dateBuild: '2018-01-12T09:06:03.261Z' };
+window.AppInfo = { name: 'svg-loader-es6-example', version: '1.0.0', build: '1515753595496', dateBuild: '2018-01-12T10:39:55.496Z' };
