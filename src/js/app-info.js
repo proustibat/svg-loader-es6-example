d361a34af34165f90bd1d1471b5bc2b74b3914d2
Jennifer Proust <jennifer.proust@gmail.com>
2018-02-04 21:16:13 +0100

    build(NPM): remove package-lock.json

--- src/js/app-info.js
+++ src/js/app-info.js
@@ -1,1 +1,1 @@
-window.AppInfo = { name: 'svg-loader-es6-example', version: '1.0.2', build: '1517773836794', dateBuild: '2018-02-04T19:50:36.794Z' };
+window.AppInfo = { name: 'svg-loader-es6-example', version: '1.0.2', build: '1517775316188', dateBuild: '2018-02-04T20:15:16.188Z' };
