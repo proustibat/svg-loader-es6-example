c352c3ce060b5581f907a228f0c0ca1fce159ddc
Jennifer Proust <jennifer.proust@gmail.com>
2018-02-05 11:38:21 +0100

    chore(NPM): Update yarn.lock after eslint-plugin-node update

--- src/js/app-info.js
+++ src/js/app-info.js
@@ -1,1 +1,1 @@
-window.AppInfo = { name: 'svg-loader-es6-example', version: '1.0.2', build: '1517775316188', dateBuild: '2018-02-04T20:15:16.188Z' };
+window.AppInfo = { name: 'svg-loader-es6-example', version: '1.0.2', build: '1517827059898', dateBuild: '2018-02-05T10:37:39.898Z' };
