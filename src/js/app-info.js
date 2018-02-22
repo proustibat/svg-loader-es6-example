60a3c40a380b1e9bcf34df7de2e7f4d187304a94
Jennifer Proust <jennifer.proust@gmail.com>
2018-02-22 15:15:56 +0100

    build(yarn.lock): update after several dependencies updates

--- src/js/app-info.js
+++ src/js/app-info.js
@@ -1,1 +1,1 @@
-window.AppInfo = { name: 'svg-loader-es6-example', version: '1.0.2', build: '1517827059898', dateBuild: '2018-02-05T10:37:39.898Z' };
+window.AppInfo = { name: 'svg-loader-es6-example', version: '1.0.2', build: '1519308916662', dateBuild: '2018-02-22T14:15:16.662Z' };
