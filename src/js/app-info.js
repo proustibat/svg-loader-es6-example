6a8305a3f8c7bba485098130644b9bc4233423e5
prousitbat <jennifer.proust@gmail.com>
2024-11-25 19:10:43 +0100

    chore(package): url-loader

--- src/js/app-info.js
+++ src/js/app-info.js
@@ -1,1 +1,1 @@
-window.AppInfo = { name: 'svg-loader-es6-example', version: '1.0.2', build: '1732557672708', dateBuild: '2024-11-25T18:01:12.708Z' };
+window.AppInfo = { name: 'svg-loader-es6-example', version: '1.0.2', build: '1732558215994', dateBuild: '2024-11-25T18:10:15.994Z' };
