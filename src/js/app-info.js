ab8b7952d02e0db2155342d73e65d76c41991fdb
prousitbat <jennifer.proust@gmail.com>
2024-11-25 18:28:56 +0100

    chore(package): update clipboard

--- src/js/app-info.js
+++ src/js/app-info.js
@@ -1,1 +1,1 @@
-window.AppInfo = { name: 'svg-loader-es6-example', version: '1.0.2', build: '1732555585514', dateBuild: '2024-11-25T17:26:25.514Z' };
+window.AppInfo = { name: 'svg-loader-es6-example', version: '1.0.2', build: '1732555691856', dateBuild: '2024-11-25T17:28:11.856Z' };
