599229c96bdfa5d9191b52fec0710cf9eac80f7e
prousitbat <jennifer.proust@gmail.com>
2024-11-25 18:31:01 +0100

    chore(package): update tingle

--- src/js/app-info.js
+++ src/js/app-info.js
@@ -1,1 +1,1 @@
-window.AppInfo = { name: 'svg-loader-es6-example', version: '1.0.2', build: '1732555691856', dateBuild: '2024-11-25T17:28:11.856Z' };
+window.AppInfo = { name: 'svg-loader-es6-example', version: '1.0.2', build: '1732555811768', dateBuild: '2024-11-25T17:30:11.768Z' };
