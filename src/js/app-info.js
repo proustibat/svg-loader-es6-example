a847833d22ac85a8ab6d9e6ab5a2e04d1d57cada
prousitbat <jennifer.proust@gmail.com>
2024-11-25 17:52:22 +0100

    chore(package): update webpack-dev-server

--- src/js/app-info.js
+++ src/js/app-info.js
@@ -1,1 +1,1 @@
-window.AppInfo = { name: 'svg-loader-es6-example', version: '1.0.2', build: '1732553307194', dateBuild: '2024-11-25T16:48:27.194Z' };
+window.AppInfo = { name: 'svg-loader-es6-example', version: '1.0.2', build: '1732553484694', dateBuild: '2024-11-25T16:51:24.694Z' };
