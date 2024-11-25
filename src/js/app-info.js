92140812e8c0fbeeacc49af56dabff15a4b12942
prousitbat <jennifer.proust@gmail.com>
2024-11-25 18:57:48 +0100

    chore(package): html-webpack-plugin

--- src/js/app-info.js
+++ src/js/app-info.js
@@ -1,1 +1,1 @@
-window.AppInfo = { name: 'svg-loader-es6-example', version: '1.0.2', build: '1732556495505', dateBuild: '2024-11-25T17:41:35.505Z' };
+window.AppInfo = { name: 'svg-loader-es6-example', version: '1.0.2', build: '1732557176399', dateBuild: '2024-11-25T17:52:56.399Z' };
