ae025511b39a64d2f650ea70833cc94cb8061150
prousitbat <jennifer.proust@gmail.com>
2024-11-25 17:46:40 +0100

    build(node): update node version

--- src/js/app-info.js
+++ src/js/app-info.js
@@ -1,1 +1,1 @@
-window.AppInfo = { name: 'svg-loader-es6-example', version: '1.0.2', build: '1519308916662', dateBuild: '2018-02-22T14:15:16.662Z' };
+window.AppInfo = { name: 'svg-loader-es6-example', version: '1.0.2', build: '1732553067775', dateBuild: '2024-11-25T16:44:27.775Z' };
