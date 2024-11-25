23a0ad6126d22a541c46ec98c2b82aca70be9a68
prousitbat <jennifer.proust@gmail.com>
2024-11-25 18:31:53 +0100

    chore(package): update tinycolor2

--- src/js/app-info.js
+++ src/js/app-info.js
@@ -1,1 +1,1 @@
-window.AppInfo = { name: 'svg-loader-es6-example', version: '1.0.2', build: '1732555811768', dateBuild: '2024-11-25T17:30:11.768Z' };
+window.AppInfo = { name: 'svg-loader-es6-example', version: '1.0.2', build: '1732555881706', dateBuild: '2024-11-25T17:31:21.706Z' };
