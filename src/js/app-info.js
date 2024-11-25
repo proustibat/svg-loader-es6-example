32a99bd2670e0092e7b353c191b4807625ff3da7
prousitbat <jennifer.proust@gmail.com>
2024-11-25 17:50:16 +0100

    chore(package): update node-sass

--- src/js/app-info.js
+++ src/js/app-info.js
@@ -1,1 +1,1 @@
-window.AppInfo = { name: 'svg-loader-es6-example', version: '1.0.2', build: '1732553067775', dateBuild: '2024-11-25T16:44:27.775Z' };
+window.AppInfo = { name: 'svg-loader-es6-example', version: '1.0.2', build: '1732553307194', dateBuild: '2024-11-25T16:48:27.194Z' };
