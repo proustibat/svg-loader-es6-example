4fdb64c99a2b3d4330032e2fc63f15520186253f
prousitbat <jennifer.proust@gmail.com>
2024-11-25 18:14:53 +0100

    chore(package): update stylelint

--- src/js/app-info.js
+++ src/js/app-info.js
@@ -1,1 +1,1 @@
-window.AppInfo = { name: 'svg-loader-es6-example', version: '1.0.2', build: '1732554595682', dateBuild: '2024-11-25T17:09:55.682Z' };
+window.AppInfo = { name: 'svg-loader-es6-example', version: '1.0.2', build: '1732554825496', dateBuild: '2024-11-25T17:13:45.496Z' };
