07e23d8ccb62d645a1d1104ef3f2e80da80bdec9
prousitbat <jennifer.proust@gmail.com>
2024-11-26 10:47:32 +0100

    chore(package): update babel-core

--- src/js/app-info.js
+++ src/js/app-info.js
@@ -1,1 +1,1 @@
-window.AppInfo = { name: 'svg-loader-es6-example', version: '2.0.0', build: '1732613100833', dateBuild: '2024-11-26T09:25:00.833Z' };
+window.AppInfo = { name: 'svg-loader-es6-example', version: '2.0.0', build: '1732614401073', dateBuild: '2024-11-26T09:46:41.073Z' };
