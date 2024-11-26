77cb41343be339e1c53121379fa68902512efc20
prousitbat <jennifer.proust@gmail.com>
2024-11-26 11:51:33 +0100

    chore(package): update webpack-merge

--- src/js/app-info.js
+++ src/js/app-info.js
@@ -1,1 +1,1 @@
-window.AppInfo = { name: 'svg-loader-es6-example', version: '2.0.0', build: '1732618156108', dateBuild: '2024-11-26T10:49:16.108Z' };
+window.AppInfo = { name: 'svg-loader-es6-example', version: '2.0.0', build: '1732618241015', dateBuild: '2024-11-26T10:50:41.015Z' };
