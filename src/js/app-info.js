1cbc690985236c384b15e4964319ac8d024c8a5f
prousitbat <jennifer.proust@gmail.com>
2024-11-26 10:55:21 +0100

    chore(package): update babel-eslint

--- src/js/app-info.js
+++ src/js/app-info.js
@@ -1,1 +1,1 @@
-window.AppInfo = { name: 'svg-loader-es6-example', version: '2.0.0', build: '1732614401073', dateBuild: '2024-11-26T09:46:41.073Z' };
+window.AppInfo = { name: 'svg-loader-es6-example', version: '2.0.0', build: '1732614888840', dateBuild: '2024-11-26T09:54:48.840Z' };
