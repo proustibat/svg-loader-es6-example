cedcd14b28a607af1b693e147281a346973d0276
prousitbat <jennifer.proust@gmail.com>
2024-11-26 12:44:35 +0100

    chore(package): update clean-webpack-plugin

--- src/js/app-info.js
+++ src/js/app-info.js
@@ -1,1 +1,1 @@
-window.AppInfo = { name: 'svg-loader-es6-example', version: '2.0.0', build: '1732621261090', dateBuild: '2024-11-26T11:41:01.090Z' };
+window.AppInfo = { name: 'svg-loader-es6-example', version: '2.0.0', build: '1732621443619', dateBuild: '2024-11-26T11:44:03.619Z' };
