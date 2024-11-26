4178db9fc2041fdc03644bdf2c35c7c5c6f18c26
prousitbat <jennifer.proust@gmail.com>
2024-11-26 12:46:21 +0100

    chore(package): update reset-css

--- src/js/app-info.js
+++ src/js/app-info.js
@@ -1,1 +1,1 @@
-window.AppInfo = { name: 'svg-loader-es6-example', version: '2.0.0', build: '1732621443619', dateBuild: '2024-11-26T11:44:03.619Z' };
+window.AppInfo = { name: 'svg-loader-es6-example', version: '2.0.0', build: '1732621561066', dateBuild: '2024-11-26T11:46:01.066Z' };
